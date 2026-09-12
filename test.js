2a26d29773540deae549733b61e729b846535733
Elvan Parthasarathy <elvan@example.com>
2026-09-12 16:13:32 +0530

    chore: remove test files

--- test.js
+++
@@ -1,1 +0,0 @@
-const { app } = require('electron'); app.whenReady().then(() = const sm = new (require('./services/SettingsManager.cjs'))(app, require('electron').nativeTheme); console.log(sm.getMediaFolder()); app.quit(); })  
